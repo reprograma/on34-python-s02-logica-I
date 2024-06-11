4927d695960f4c590ac03d91f88d4e5eb141523b
Williane Fernandes <williane.sena.fernandes@gmail.com>
2024-06-11 20:25:06 -0300

    add os arquivos

---
+++ on34-python-s02/operadores.py
@@ -0,0 +1,13 @@
+#Existem para os tipos int e float
+
+print(1+3) #soma
+print(10/3) #divisao
+print(3**2) #exponenciacao
+print(pow(3, 2)) #exponenciacao
+
+print(10%3) #divisao com resto
+
+valor = 10/3
+valor_arredondado = round(valor, 2) #arredondar valores
+print(valor_arredondado)
+
