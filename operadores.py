960596d0edb47cd1cab7fb3eb60727fee0dff1fc
dudaa28 <eduardaq2805@gmail.com>
2024-06-11 20:22:11 -0300

    Minha segunda atividade

---
+++ operadores.py
@@ -0,0 +1,48 @@
+# Existem para os tipos int e float
+
+print(1+3) #soma
+print(30-90) #subtração
+print(10/3) #multiplicação
+print(pow(3, 2)) #divisão
+print(3**2) #exponenciação
+print(10%3) #mod (divisão com resto)
+
+valor = 10/3
+valor_arredondado = round(valor, 2) #roud
+print(valor_arredondado)
+
+# 10/06
+
+#atribuição
+
+x = 5
+print(x)
+
+#adição na atribuição
+
+x += 1
+print(x)
+
+#subtração na atribuição
+subtração = 5
+subtração -= 3
+print(subtração)
+
+#multiplicação
+multi = 4
+multi *= 3
+print(multi)
+
+#divisão
+
+divisao = 121
+divisao /=11
+print(divisao)
+
+# divisao inteira na atribuição
+
+divisao_inteira = 10
+divisao_inteira //=3
+
+
+
---
+++ operadores_logicos.py
@@ -0,0 +1,26 @@
+#operador AND
+
+and_operador = True and True
+print(and_operador)
+
+and_operador = True and False
+print(and_operador)
+
+and_operador = False and False
+print(and_operador)
+
+and_operador = (13%2==0) and (True) and (44>23)
+print(and_operador)
+
+#operador OR
+
+or_operador = True or False
+print(or_operador)
+or_operador = True or True
+print(or_operador)
+or_operador = False or False
+print(or_operador)
+
+#operador NOT
+print(not True)
+print(not False)
