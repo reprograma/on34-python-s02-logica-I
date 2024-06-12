f09cb6d87d73220c73e0291ad4d8688293ffed6f
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-06-12 00:48:38 -0300

    Adicionando função de verificação de número ímpar na calculadora

--- Lethicia-Asevedo/calculadora-divisao.py
+++ Lethicia-Asevedo/calculadora-divisao.py
@@ -18,5 +18,13 @@ x = (int(input("Digite o primeiro número: ")))
 y = (int(input("Digite o segundo número: ")))
 
 resultado = x / y 
+numero_impar = resultado % 2
+
 
 print("O resultado da sua divisão é: ", resultado)
+
+# função para definir se o número resultante é ímpar ou não. 
+if numero_impar == 1:
+    print("O valor resultante é um número ímpar")
+else:
+    print("O valor resultante não é um número ímpar")
--- Lethicia-Asevedo/calculadora-potenciacao.py
+++ Lethicia-Asevedo/calculadora-potenciacao.py
@@ -17,6 +17,13 @@ print("""
 x = (int(input("Digite o número da base: ")))
 y = (int(input("Digite o número exponente: ")))
 
-resultado = x ** y 
+resultado = x ** y
+numero_impar = resultado % 2
 
 print("O resultado da sua potenciação é: ", resultado)
+
+# função para definir se o número resultante é ímpar ou não. 
+if numero_impar == 1:
+    print("O valor resultante é um número ímpar")
+else:
+    print("O valor resultante não é um número ímpar")
