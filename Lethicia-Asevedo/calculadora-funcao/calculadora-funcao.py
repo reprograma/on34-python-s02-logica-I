74bf266976aab49489da765f08c82d2778c8da3d
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-06-12 22:27:14 -0300

    Adicionando a calculadora construida com funções e reorganizando pastas

---
+++ Lethicia-Asevedo/calculadora-funcao/calculadora-funcao.py
@@ -0,0 +1,87 @@
+print("""
+░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░██████╗░░█████╗░██████╗░░█████╗░
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║██║░░██║██║░░██║██████╔╝███████║
+██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║██║░░██║██║░░██║██╔══██╗██╔══██║
+╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║██████╔╝╚█████╔╝██║░░██║██║░░██║
+░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+""")
+
+
+def soma(x,y):
+    return x + y
+
+x = (int(input("Digite o primeiro valor: ")))
+y = (int(input("Digite o segundo valor: ")))
+resultado = soma(x,y)
+impar = resultado %2
+print("O resultado da sua soma é", resultado)
+
+if resultado % 2 == 0:
+    print("O valor resultante da sua operação não é um número ímpar")
+else:
+    print("O valor resultante da sua operação é ímpar")
+
+
+
+
+def subtracao(x,y):
+    return x - y 
+x = (int(input("Digite o primeiro valor: ")))
+y = (int(input("Digite o segundo valor: ")))
+resultado = subtracao(x,y)
+impar = resultado %2
+print("O resultado da sua subtração é", resultado)
+
+if resultado % 2 == 0:
+    print("O valor resultante da sua operação não é um número ímpar")
+else:
+    print("O valor resultante da sua operação é ímpar")
+
+
+
+def divisao(x,y):
+    return x / y
+x = (int(input("Digite o primeiro valor: ")))
+y = (int(input("Digite o segundo valor: ")))
+resultado = divisao(x,y)
+impar = resultado %2
+print("O resultado da sua divisão é", resultado)
+
+if resultado % 2 == 0:
+    print("O valor resultante da sua operação não é um número ímpar")
+else:
+    print("O valor resultante da sua operação é ímpar")
+
+
+
+def multiplicacao(x,y):
+    return x * y 
+x = (int(input("Digite o primeiro valor: ")))
+y = (int(input("Digite o segundo valor: ")))
+resultado = multiplicacao(x,y)
+impar = resultado %2
+print("O resultado da sua multiplicação é", resultado)
+
+if resultado % 2 == 0:
+    print("O valor resultante da sua operação não é um número ímpar")
+else:
+    print("O valor resultante da sua operação é ímpar")
+
+
+def potenciacao(x,y):
+    return x ** y
+x = (int(input("Digite o primeiro valor: ")))
+y = (int(input("Digite o segundo valor: ")))
+resultado = potenciacao(x,y)
+impar = resultado %2
+print("O resultado da sua potenciação é", resultado)
+
+if resultado % 2 == 0:
+    print("O valor resultante da sua operação não é um número ímpar")
+else:
+    print("O valor resultante da sua operação é ímpar")
+
+
+
+
