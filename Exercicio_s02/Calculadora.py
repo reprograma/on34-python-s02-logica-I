714787f4f64d19477b569bd0ddcd19780e98adc0
Verônica Toledo <veronica.toledobm@gmail.com>
2024-06-14 12:54:52 -0300

    Definindo a função impar

--- Exercicio_s02/Calculadora.py
+++ Exercicio_s02/Calculadora.py
@@ -1,7 +1,5 @@
 """
-O usuário entrará com dois valores, e será necessário realizar o cálculo deles através destas funções com:
-
-Trazer se o número resultante de cada operação é ímpar.
+O usuário entrará com dois valores, e será necessário realizar o cálculo deles através destas funções:
 
 """
 # Funções
@@ -11,25 +9,42 @@ def somar (x, y):
     Resultado = x + y
     print (Resultado)
 
+    impar (Resultado)
+
 # subtrair(x, y): recebe dois números e retorna a subtração.
 def subtrair (x, y):
     Resultado = x - y
     print (Resultado)
+
+    impar (Resultado)
 
 # multiplicar(x, y): recebe dois números e retorna a multiplicação.
 def multiplicar (x, y):
     Resultado = x * y
     print (Resultado)
 
+    impar (Resultado)
+
 # dividir(x, y): recebe dois números e retorna a divisão do primeiro pelo segundo.
 def dividir (x, y):
     Resultado = x / y
     print (Resultado)
 
+    impar (Resultado)
+
 # potenciação(x, y): receber dois números e elevar o primeiro pelo segundo.
 def potenciacao (x, y):
     Resultado = x ** y
     print (Resultado)
+
+    impar (Resultado)
+
+# Função que verifica se o resultado é impar
+def impar(Resultado):
+    if Resultado % 2 == 0:
+        print (Resultado, " É par")
+    else:
+        print (Resultado, " É impar")
 
 # Entrada dos Valores
 x = int(input("Digite um número"))
