b4657ff37ae0fdde5e3035951df17bff5f133c63
Verônica Toledo <veronica.toledobm@gmail.com>
2024-06-14 12:31:34 -0300

    comentando funções

--- Exercicio_s02/Calculadora.py
+++ Exercicio_s02/Calculadora.py
@@ -1,31 +1,32 @@
 """
 O usuário entrará com dois valores, e será necessário realizar o cálculo deles através destas funções com:
 
-somar(x, y): recebe dois números e retorna a soma.
-subtrair(x, y): recebe dois números e retorna a subtração.
-multiplicar(x, y): recebe dois números e retorna a multiplicação.
-dividir(x, y): recebe dois números e retorna a divisão do primeiro pelo segundo.
-potenciação(x, y): receber dois números e elevar o primeiro pelo segundo.
 Trazer se o número resultante de cada operação é ímpar.
 
 """
 # Funções
-def soma (x, y):
+
+# somar(x, y): recebe dois números e retorna a soma.
+def somar (x, y):
     Resultado = x + y
     print (Resultado)
 
+# subtrair(x, y): recebe dois números e retorna a subtração.
 def subtrair (x, y):
     Resultado = x - y
     print (Resultado)
 
+# multiplicar(x, y): recebe dois números e retorna a multiplicação.
 def multiplicar (x, y):
     Resultado = x * y
     print (Resultado)
 
+# dividir(x, y): recebe dois números e retorna a divisão do primeiro pelo segundo.
 def dividir (x, y):
     Resultado = x / y
     print (Resultado)
 
+# potenciação(x, y): receber dois números e elevar o primeiro pelo segundo.
 def potenciacao (x, y):
     Resultado = x ** y
     print (Resultado)
