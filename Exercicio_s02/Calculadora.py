0280413cf30e95a81ed1961ffd87cf7a761af634
Verônica Toledo <veronica.toledobm@gmail.com>
2024-06-14 12:15:11 -0300

    Definindo a função soma

---
+++ Exercicio_s02/Calculadora.py
@@ -0,0 +1,21 @@
+"""
+O usuário entrará com dois valores, e será necessário realizar o cálculo deles através destas funções com:
+
+somar(x, y): recebe dois números e retorna a soma.
+subtrair(x, y): recebe dois números e retorna a subtração.
+multiplicar(x, y): recebe dois números e retorna a multiplicação.
+dividir(x, y): recebe dois números e retorna a divisão do primeiro pelo segundo.
+potenciação(x, y): receber dois números e elevar o primeiro pelo segundo.
+Trazer se o número resultante de cada operação é ímpar.
+
+"""
+# Funções
+def soma (x, y):
+    Resultado = x+y
+    print (Resultado)
+
+# Entrada dos Valores
+x = int(input("Digite um número"))
+y = int(input("Digite outro número"))
+
+
