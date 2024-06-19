38c24c2a64ba8ded08abe06b332408909dc2b311
floracanabrava <floracanabrava@gmail.com>
2024-06-19 20:46:19 -0300

    Delete floracanabrava directory

--- floracanabrava/cadastro.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-nome = input('Digite seu nome: ')
-idade = int(input('Digite sua idade: '))
-
-print(nome, ", idade: ", idade)
-
-print("olá ", nome)
--- floracanabrava/entradas.py
+++
@@ -1,9 +0,0 @@
-entrada1 = input("Informe o primeiro valor: ")
-entrada2 = input("Informe o segundo valor: ")
-
-print(type(entrada1))
-print(type(entrada2))
-
-concat = entrada1+entrada2
-print("valor da vaiavel", concat)
-print("tipo do concat",  type(concat))
